21724498a474f840f18aea1e953a0f0fb68a0725
Kaustav Dedhia <ikaustavdedhia@gmail.com>
2026-04-01 15:22:18 +0530

    Update model1.py

--- model1.py
+++ model1.py
@@ -10,17 +10,18 @@ lr = LinearRegression()
 for _ in range(10):
     rng = np.random.RandomState(_)
     x = 10 * rng.rand(1000).reshape(-1,1)
-    y = 2 * x - 5 + rng.randn(1000).reshape(-1,1)
+    y = 2 * x - 5 + rng.randn(1000).reshape(-1,1)  
     X_train, X_test, y_train, y_test = train_test_split(
-        x, y, test_size=0.2, random_state=50
+    x, y, test_size=0.2, random_state=50
     )
     # Fitting the model
     lr.fit(X_train, y_train)
     y_preds = lr.predict(X_test)
     test_mse = mean_squared_error(y_test, y_preds)
-    average_mse = np.mean(test_mse)
-    print(f'MSE Result: {test_mse}')
-    print("Average Mean Squared Error:", average_mse)
+
+average_mse = np.mean(test_mse)
+print(f'MSE Result: {test_mse}')
+print("Average Mean Squared Error:", average_mse)
 ############################################################################
 joblib.dump(lr, "model1.pkl")
 ############################################################################
